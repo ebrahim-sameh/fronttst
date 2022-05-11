6a36c382ff67c88ab7d0b4043dcf0fe20338f63b
Ebrahim Sameh <ebrahimsameh16@gmail.com>
2022-05-11 22:29:18 +0200

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -1,6 +1,6 @@
 import Medusa from "@medusajs/medusa-js";
 
 const BACKEND_URL =
-  process.env.NEXT_PUBLIC_MEDUSA_URL || "http://medusa-test-app9.herokuapp.com";
+  process.env.NEXT_PUBLIC_MEDUSA_URL || "https://medusa-test-app9.herokuapp.com";
 
 export const createClient = () => new Medusa({ baseUrl: BACKEND_URL });
