9f136922a35823271de945bcaeae700be953bb32
Ebrahim Sameh <ebrahimsameh16@gmail.com>
2022-05-11 21:41:45 +0200

    qqq

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,6 @@ export default function Home({ products }) {
           <h1 className={styles.title}>
             Medusa + Next.js Starter{" "}
             <span role="img" aria-label="Rocket emoji">
-              <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
               🚀
             </span>
           </h1>
