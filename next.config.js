143e53798c9f232c38a79fb866b8c2034de9c8da
demo55oo <85605417+demo55oo@users.noreply.github.com>
2022-05-18 19:13:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com"],
+    domains: ["catzatst.s3.ap-southeast-1.amazonaws.com"],
   },
 };
